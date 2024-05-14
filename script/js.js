2de9618915d573e9ee57f17b2bc8ae782bc78d2f
j.kim <j.kim@gxp.co.jp>
2024-05-14 15:44:59 +0900

    first commit

---
+++ script/js.js
@@ -0,0 +1,13 @@
+let myNumber = "500";
+console.log(typeof myNumber);
+
+myNumber = 500;
+console.log(typeof myNumber);
+
+// let const = 1;
+
+const name = { firstname: "kim" };
+console.log(name.firstname);
+name.firstname = "lee";
+
+console.log(name.firstname);
